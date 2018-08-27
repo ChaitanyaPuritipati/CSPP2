2d87d8a58a09ffcae3115fb001ce2ca0ed01566e
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-27 20:28:44 +0530

    Added Activity 14 in CSPP2

---
+++ cspp2-Practice/m1/Activity 14/degrees_to_fahrenheit.java
@@ -0,0 +1,9 @@
+public class degrees_to_fahrenheit
+{
+	public static void main(String[] args)
+	{
+		double degrees = 32;
+		double farenheit = (degrees*1.8) + 32.0;
+		System.out.println(farenheit);
+	}
+}
