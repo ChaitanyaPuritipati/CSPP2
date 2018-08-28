074717cf2725a46d92b68bae2d9ffa1cf3d3a0d7
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-28 17:15:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cssp2-Assignments/m2/Assignment-3/Solution.java
+++ cssp2-Assignments/m2/Assignment-3/Solution.java
@@ -33,8 +33,8 @@ final class Solution {
         if (exponent == 0) {
             return 1;
         } else {
-            return base*power(base, exponent - 1);
-        } 
+            return base * power(base, exponent - 1);
+        }
 
     }
 }
