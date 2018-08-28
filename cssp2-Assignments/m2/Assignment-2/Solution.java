fdfde9617a959f84c03bce8aebcb4a3c6fd51921
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-28 16:12:30 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m2/Assignment-2/Solution.java
+++ cssp2-Assignments/m2/Assignment-2/Solution.java
@@ -3,40 +3,41 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/*
-		Main function starts here.
-		*/
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	static void rootsOfQuadraticEquation(final int a, final int b, final int c){
-		double determinant = Math.sqrt((b * b) - (4 * a * c));
-		if (determinant >= 0) {
-			double root1 = ((- b) + determinant)/(2 * a);
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /*
+        Main function starts here.
+        */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    static void rootsOfQuadraticEquation(final int a, final int b,
+        final int c){
+        double determinant = Math.sqrt((b * b) - (4 * a * c));
+        if (determinant >= 0) {
+            double root1 = ((- b) + determinant)/(2 * a);
             double root2 = ((- b) - determinant)/(2 * a);
             System.out.println(root1 + " " + root2);
-		}
-	}
+        }
+    }
 }
