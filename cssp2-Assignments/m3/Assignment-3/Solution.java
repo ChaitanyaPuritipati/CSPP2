5f00d7ef9003cfef745e77ae428063519bb6d04b
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-29 17:06:17 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cssp2-Assignments/m3/Assignment-3/Solution.java
+++ cssp2-Assignments/m3/Assignment-3/Solution.java
@@ -1,8 +1,8 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 public class Solution {
 
     public static void main(String[] args) {
@@ -10,15 +10,15 @@ public class Solution {
         Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int small = Math.min(n1, n2);
-        int large = Math.max(n1, n2);
+        final int small = Math.min(n1, n2);
+        final int large = Math.max(n1, n2);
         System.out.println(gcd(small,large));
     }
-    static int gcd(int a, int b){
+    static int gcd(final int a, final int b){
         for (int i = a; i>=2; i--) {
-        	if (b%i == 0 && a%i == 0) {
-        		return i;
-        	}
+            if (b%i == 0 && a%i == 0) {
+                return i;
+            }
 
         }
         return 1;
