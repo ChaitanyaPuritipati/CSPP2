309c50b28cecc276a944a2f5189a65cc973d6709
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-29 16:42:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cssp2-Assignments/m3/Assignment-3/Solution.java
+++ cssp2-Assignments/m3/Assignment-3/Solution.java
@@ -12,13 +12,11 @@ public class Solution {
         int n2 = s.nextInt();
         int small = Math.min(n1, n2);
         int large = Math.max(n1, n2);
-        System.out.println(small);
-        System.out.println(large);
         System.out.println(gcd(small,large));
     }
     static int gcd(int a, int b){
-        for (int i = a; i>= 2; i--) {
-        	if (b%i == 0) {
+        for (int i = a; i>=2; i--) {
+        	if (b%i == 0 && a%i == 0) {
         		return i;
         	}
 
