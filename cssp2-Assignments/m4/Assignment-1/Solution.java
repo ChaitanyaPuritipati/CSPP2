b188c03b29b5def86663a79255896f23ceb06a24
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 13:44:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-1/Solution.java
+++ cssp2-Assignments/m4/Assignment-1/Solution.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * { Main function is used to start the execution of the program }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		final int n = sc.nextInt();
-		int[] elements = new int[n];
-		int max = 0;
- 		for (int i = 0; i < n; i++) {
- 			elements[i] = sc.nextInt();
- 			if (elements[i] > max) {
- 				max = elements[i];
- 			}
-		}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main function is used to start the execution of the program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        int[] elements = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            elements[i] = sc.nextInt();
+            if (elements[i] > max) {
+                max = elements[i];
+            }
+        }
         System.out.println(max);
-	}
+    }
 }
