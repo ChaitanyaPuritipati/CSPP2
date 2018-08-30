b619ca63d621a52c4e1e23c39b00d593e6525673
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 11:48:25 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cssp2-Assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int test = input.nextInt();
+		System.out.println(test); 
+	}
+}
