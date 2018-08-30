f813fcc702169367d874dfca32fb146718135da8
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 12:20:13 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-2/Solution.java
+++ cssp2-Assignments/m4/Assignment-2/Solution.java
@@ -9,9 +9,31 @@ public class Solution {
    		for (int row=0; row < rows_1; row++) {
    			for (int col=0;col<columns_1;col++) {
    				arr1[row][col] = input.nextInt();
-   				System.out.println(arr1[row][col]);
    			}
    		}
-   		
+   		int rows_2 = input.nextInt();
+   		int columns_2 = input.nextInt();
+   		int arr2[][] = new int[rows_2][columns_2];
+   		for (int row=0; row < rows_1; row++) {
+   			for (int col=0;col<columns_1;col++) {
+   				arr2[row][col] = input.nextInt();
+   			}
+   		}
+   		if (rows_1 != rows_2 && columns_1 != columns_2) {
+   			System.out.println("not possible");
+   			return;
+   		}
+   		int addarr[][] = new int[rows_1][columns_1];
+   		for (int row=0; row < rows_1; row++) {
+   			for (int col=0;col<columns_1;col++) {
+   				addarr[row][col] = arr1[row][col] + arr2[row][col];
+   			}
+   		}
+   		for (int row=0; row < rows_1; row++) {
+   			for (int col=0;col<columns_1;col++) {
+   				System.out.print(addarr[row][col] + " ");
+   			} 
+   			System.out.println();
+   		}
 	}
 }
