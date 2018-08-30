22a4f5dd4f771e5eca7a13b1947d4540516404da
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 12:04:32 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-2/Solution.java
+++ cssp2-Assignments/m4/Assignment-2/Solution.java
@@ -3,9 +3,16 @@ public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int rows = input.nextInt();
-		int columns = input.nextInt();
-		System.out.println(rows);
-		System.out.println(columns);
+		int rows_1 = input.nextInt();
+   		int columns_1 = input.nextInt();
+   		int rowarr[] = new int[rows_1];
+   		for (int row=0; row < rows_1; row++) {
+   			int colarr[] = new int[columns_1]; 
+   			for (int col=0;col<columns_1;col++) {
+   				colarr[col] = input.nextInt();
+   			}
+   			System.out.println(colarr);
+   		}
+   		System.out.println(rowarr);
 	}
 }
