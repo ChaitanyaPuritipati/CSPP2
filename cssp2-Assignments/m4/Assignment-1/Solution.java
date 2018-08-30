346bae5a5657797efa96b76016a94a1b72a9953d
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 13:43:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-1/Solution.java
+++ cssp2-Assignments/m4/Assignment-1/Solution.java
@@ -1,13 +1,24 @@
 
-import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused
+	}
+	/**
+	 * { Main function is used to start the execution of the program }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		final int n = sc.nextInt();
 		int[] elements = new int[n];
 		int max = 0;
  		for (int i = 0; i < n; i++) {
