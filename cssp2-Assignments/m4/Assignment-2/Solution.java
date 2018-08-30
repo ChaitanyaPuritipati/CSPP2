e0e5e1f987c22208420a5d78fcd66cbd3f6c504b
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 13:28:10 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-2/Solution.java
+++ cssp2-Assignments/m4/Assignment-2/Solution.java
@@ -14,39 +14,39 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
-        final int rows_1 = input.nextInt();
-        final int columns_1 = input.nextInt();
-        int arr1[][] = new int[rows_1][columns_1];
-        for (int row = 0; row < rows_1; row++) {
-            for (int col = 0; col < columns_1; col++) {
+        final int rows1 = input.nextInt();
+        final int columns1 = input.nextInt();
+        int arr1[][] = new int[rows1][columns1];
+        for (int row = 0; row < rows1; row++) {
+            for (int col = 0; col < columns1; col++) {
                 arr1[row][col] = input.nextInt();
             }
         }
-        final int rows_2 = input.nextInt();
-        final int columns_2 = input.nextInt();
-        int arr2[][] = new int[rows_2][columns_2];
-        for (int row=0; row < rows_2; row++) {
-            for (int col=0;col<columns_2;col++) {
+        final int rows2 = input.nextInt();
+        final int columns2 = input.nextInt();
+        int arr2[][] = new int[rows2][columns2];
+        for (int row=0; row < rows2; row++) {
+            for (int col=0; col < columns2; col++) {
                 arr2[row][col] = input.nextInt();
             }
         }
-        if (rows_1 != rows_2 && columns_1 != columns_2) {
+        if (rows1 != rows2 && columns1 != columns2) {
             System.out.println("not possible");
             return;
         }
-        int addarr[][] = new int[rows_1][columns_1];
-        for (int row = 0; row < rows_1; row++) {
-            for (int col = 0; col < columns_1; col++) {
+        int addarr[][] = new int[rows1][columns1];
+        for (int row = 0; row < rows1; row++) {
+            for (int col = 0; col < columns1; col++) {
                 addarr[row][col] = arr1[row][col] + arr2[row][col];
             }
         }
-        for (int row = 0; row < rows_1; row++) {
-            for (int col = 0; col < columns_1 - 1; col++) {
+        for (int row = 0; row < rows1; row++) {
+            for (int col = 0; col < columns1 - 1; col++) {
                 System.out.print(addarr[row][col] + " ");
             }
-            System.out.print(addarr[row][columns_1-1]);
+            System.out.print(addarr[row][columns1 - 1]);
             System.out.println();
         }
     }
