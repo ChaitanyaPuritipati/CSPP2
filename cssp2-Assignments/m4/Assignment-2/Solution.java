cee90681a7b516df69fdca20182b071effd0be20
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-30 11:49:06 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-2/Solution.java
+++ cssp2-Assignments/m4/Assignment-2/Solution.java
@@ -3,7 +3,9 @@ public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int test = input.nextInt();
-		System.out.println(test); 
+		int rows = input.nextInt();
+		int columns = input.nextInt();
+		System.out.println(rows);
+		System.out.println(columns);
 	}
 }
