2039b866963be66096888334dc25d7d59fb5ed84
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-31 14:44:11 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-3/Solution.java
+++ cssp2-Assignments/m4/Assignment-3/Solution.java
@@ -1,4 +1,4 @@
-import java.lang.Math;
+import static java.lang.Math.pow;
 // Given an String, Write a java method that returns the decimal value for the given binary string.
 import java.util.Scanner;
 public class Solution
