44e6be4d2b69a9ed8ee961c9483cbe6e98f61b7c
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-31 14:57:16 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-3/Solution.java
+++ cssp2-Assignments/m4/Assignment-3/Solution.java
@@ -3,38 +3,38 @@ import java.util.Scanner;
  * { Solution class is created }.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * { Main function is created to start the execution of the program }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner input = new Scanner(System.in);
-		int numofstrings = input.nextInt();
-		for(int i = 0; i < numofstrings; i++){
-			String binarystring = input.next();
-			String decimalstring = binaryToDecimal(binarystring);//Write binaryToDecimal function
-			System.out.println(decimalstring);
-		}
-	}
-	/**
-	 * { binaryToDecimal function is created to convert binary number to decimal number }.
-	 *
-	 * @param      funcstring  Binary string 
-	 *
-	 * @return     { Return value is decimal string }.
-	 */
-	static String binaryToDecimal(final String funcstring) {
-		int decimalnum = 0;
-		for (int i = 0; i < funcstring.length(); i++) {
-			decimalnum = decimalnum + ((Integer.parseInt (funcstring.charAt(i) + ""))*((int) Math.pow(2, funcstring.length()-i-1)));
-		}
-		return (decimalnum + "");
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main function is created to start the execution of the program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numofstrings = input.nextInt();
+        for (int i = 0; i < numofstrings; i++) {
+            String binarystring = input.next();
+            String decimalstring = binaryToDecimal(binarystring);
+            System.out.println(decimalstring);
+        }
+    }
+    /**
+     * { binaryToDecimal function is created to convert binary number to decimal number }.
+     *
+     * @param      funcstring  Binary string 
+     *
+     * @return     { Return value is decimal string }.
+     */
+    static String binaryToDecimal(final String funcstring) {
+        int decimalnum = 0;
+        for (int i = 0; i < funcstring.length(); i++) {
+            decimalnum = decimalnum + ((Integer.parseInt (funcstring.charAt(i) + ""))*((int) Math.pow(2, funcstring.length()-i-1)));
+        }
+        return (decimalnum + "");
+    }
 }
