aeb099531842f1cc64cf5a2ab85e7f3d07fad950
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-31 15:00:01 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-3/Solution.java
+++ cssp2-Assignments/m4/Assignment-3/Solution.java
@@ -34,7 +34,7 @@ final class Solution {
         int decimalnum = 0;
         for (int i = 0; i < funcstring.length(); i++) {
             int everycharint = (Integer.parseInt (funcstring.charAt(i) + ""));
-            decimalnum = decimalnum + (everycharint)*((int) Math.pow(2, funcstring.length()-i-1));
+            decimalnum = decimalnum + (everycharint) * ((int) Math.pow(2, funcstring.length() - i - 1));
         }
         return (decimalnum + "");
     }
