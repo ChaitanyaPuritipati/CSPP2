58e039e43ed937f97cbac4e869f377fb61987498
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-31 14:36:16 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-3/Solution.java
+++ cssp2-Assignments/m4/Assignment-3/Solution.java
@@ -9,7 +9,7 @@ public class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		for(int i = 0; i <= n; i++){
+		for(int i = 0; i < n; i++){
 			String s = sc.nextLine();
 			System.out.println(s);
 			String res = binaryToDecimal(s);//Write binaryToDecimal function
