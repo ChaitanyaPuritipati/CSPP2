2eae847af306f535ae284fa3c37d3e392c8b95bc
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-31 15:16:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cssp2-Assignments/m4/Assignment-4/Solution.java
+++ cssp2-Assignments/m4/Assignment-4/Solution.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args)
-	{
-		Scanner input = new Scanner(System.in);
-		String inputstring = input.nextLine();
-		String reversestring = reverseString(inputstring);	
-		System.out.println(reversestring);
-	}
-	static String reverseString(String funcstring) {
-		String outstring = "";
-		for (int everyindex = 0; everyindex < funcstring.length(); everyindex++) {
-			outstring = funcstring.charAt(everyindex) + outstring;
-		}
-		return outstring;
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    private Solution() {
+        //unused.
+    }
+    /**
+     * { Main function to start execution of program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String inputstring = input.nextLine();
+        String reversestring = reverseString(inputstring);  
+        System.out.println(reversestring);
+    }
+    static String reverseString(String funcstring) {
+        String outstring = "";
+        for (int everyindex = 0; everyindex < funcstring.length(); everyindex++) {
+            outstring = funcstring.charAt(everyindex) + outstring;
+        }
+        return outstring;
+    }
 }
