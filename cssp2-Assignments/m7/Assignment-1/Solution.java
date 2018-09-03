bdaeabc927f996014f025c54594d7d3dff2f9773
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-03 17:22:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cssp2-Assignments/m7/Assignment-1/Solution.java
+++ cssp2-Assignments/m7/Assignment-1/Solution.java
@@ -1,21 +1,39 @@
 import java.util.*;
+/**
+ * Class for input validator.
+ */
 class InputValidator {
-	private String testinput;
-	public InputValidator(String giveninput) {
-		this.testinput = giveninput;
-	}
-	public boolean validateData() {
-		final int six = 6;
-		return this.testinput.length() >= six;
-	}
+    /**
+     * { It is of string type }.
+     */
+    private String testinput;
+    /**
+     * Constructs the object.
+     *
+     * @param      giveninput  String type
+     */
+    public InputValidator(final String giveninput) {
+        this.testinput = giveninput;
+    }
+    /**
+     * { It is created to validate the data }.
+     *
+     * @return     { return value is of boolean type }.
+     */
+    public boolean validateData() {
+        final int six = 6;
+        return this.testinput.length() >= six;
+    }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String args[]) {
-		Scanner s = new Scanner(System.in);
-		String input = s.next();
-		InputValidator i = new InputValidator(input);
-		System.out.println(i.validateData());
+    public static void main(final String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        InputValidator inputclass = new InputValidator(input);
+        System.out.println(inputclass.validateData());
 
-	}
-
+    }
 }
