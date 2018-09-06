b2a31248c573bf9cbc9469417cf6722893bfdf61
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-06 11:11:27 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cssp2-Assignments/m10/Assignment-3/List.java
+++ cssp2-Assignments/m10/Assignment-3/List.java
@@ -157,8 +157,6 @@ public class List<E> {
      */
     public boolean contains(E item) {
         for (int i = 0; i < size; i++) {
-            // System.out.println(list[i]);
-            // System.out.println(item);
             if (list[i].equals(item)) {
                 return true;
             }
