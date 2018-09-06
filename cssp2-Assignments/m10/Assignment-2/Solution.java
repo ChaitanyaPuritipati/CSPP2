1331331dd34df21166706ee131749e4be60125a2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-06 10:10:31 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cssp2-Assignments/m10/Assignment-2/Solution.java
+++ cssp2-Assignments/m10/Assignment-2/Solution.java
@@ -30,40 +30,41 @@ final class Solution {
             String[] tokens = line.split(" ");
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
-                case "add":
-                    l.add(tokens[1]);
+            case "add":
+                l.add(tokens[1]);
                 break;
-                case "addAll":
-                if(tokens.length==2){
-                String[] t1 = tokens[1].split(",");
-                l.addAll(t1);
+            case "addAll":
+                if (tokens.length == 2) {
+                    String[] t1 = tokens[1].split(",");
+                    l.addAll(t1);
                 }
                 break;
-                case "size":
+            case "size":
                 // invoke size method and print the list size
                 // BTW, list size is not the array size
                 // it is the number of items in the list
                 System.out.println(l.size());
                 break;
-                case "print":
+            case "print":
                 // print the list (implement toString in StringList class
                 //for this to work)
                 // expected format is [item-1,item-2,...,item-n]
                 // review the output testcase file
                 System.out.println(l);
                 break;
-                case "remove":
+            case "remove":
                 l.remove(Integer.parseInt(tokens[1]));
                 break;
-                case "indexOf":
+            case "indexOf":
                 System.out.println(l.indexOf(tokens[1]));
                 break;
-                case "get":
+            case "get":
                 System.out.println(l.get(Integer.parseInt(tokens[1])));
                 break;
-                case "contains":
+            case "contains":
                 System.out.println(l.contains(tokens[1]));
                 break;
+            default:
             }
         }
     }
