06857a5ce56f93b4f473f3189ccacb06c6964734
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-06 10:09:37 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cssp2-Assignments/m10/Assignment-2/Solution.java
+++ cssp2-Assignments/m10/Assignment-2/Solution.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * { Main function starts here }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main function starts here }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l=(StringListInterface)sl;//Typecasting is done for StringListInterface
+        StringListInterface l = (StringListInterface)sl;
+        //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -30,7 +31,7 @@ class Solution {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                 case "add":
-	                l.add(tokens[1]);
+                    l.add(tokens[1]);
                 break;
                 case "addAll":
                 if(tokens.length==2){
@@ -65,5 +66,5 @@ class Solution {
                 break;
             }
         }
-	}
+    }
 }
