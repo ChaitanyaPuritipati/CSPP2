ac5eb3cdc8b0da03c8b01cd9ed02a481315cc5d2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-06 14:34:00 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cssp2-Assignments/m10/Assignment-3/Solution.java
+++ cssp2-Assignments/m10/Assignment-3/Solution.java
@@ -341,7 +341,9 @@ final class Solution {
             }
             break;
 
-        case "O"://This case will be executed for Student type list i.e to store List of Student Objects
+        case "O":
+        //This case will be executed for Student type list
+        //i.e to store List of Student Objects
             List<Student> listStudent = new List();
             while (stdin.hasNext()) {
                 // read the line
