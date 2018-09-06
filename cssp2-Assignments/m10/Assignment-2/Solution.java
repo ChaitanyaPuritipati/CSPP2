97fcff304832829488bd9f612f3141063b072bec
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-06 10:08:21 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cssp2-Assignments/m10/Assignment-2/Solution.java
+++ cssp2-Assignments/m10/Assignment-2/Solution.java
@@ -1,7 +1,15 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+class Solution {
+	/**
+	 * { Main function starts here }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
         StringListInterface l=(StringListInterface)sl;//Typecasting is done for StringListInterface
