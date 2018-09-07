06cd95cfbe5de3a2a524c5e18026fed830c030a7
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-07 17:13:27 +0530

    "check_style_score -> 8 of 8 passed. style score: 0/1 "

--- cssp2-Assignments/m11/Assignment-2/Solution.java
+++ cssp2-Assignments/m11/Assignment-2/Solution.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 final class Student {
     /**
-     * { name variable }
+     * { name variable }.
      */
     private String name;
     /**
@@ -13,7 +13,8 @@ final class Student {
      *
      * @param      name  The name
      */
-    public Student(String name) {//A constructor used to initialize the instance variables
+    Student(String name) {
+    //A constructor used to initialize the instance variables
         this.name = name;
     }
     /**
