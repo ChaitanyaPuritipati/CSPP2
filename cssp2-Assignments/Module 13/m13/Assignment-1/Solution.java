10e9e2e74a1b2dac27b32b2a87f88ec378e87142
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-08 11:07:05 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/Module 13/m13/Assignment-1/Solution.java
+++ cssp2-Assignments/Module 13/m13/Assignment-1/Solution.java
@@ -4,190 +4,187 @@ import java.util.Arrays;
 
 /**
  * Class for set.
- * @author : 
+ * @author :
  */
 class Set {
-    //your code goes here...
-    //Good luck :-)
-    private int[] setarr;
-    private int size;
-    public Set() {
-    	final int ten = 10;
-    	setarr = new int[ten];
-    	size = 0;
-    }
-    private void resize() {
-    	setarr = Arrays.copyOf(setarr, 2 * setarr.length);
-    }
-    public int size() {
-    	return size;
-    }
-    public boolean contains(int item) {
-    	for(int i = 0; i < size; i++) {
-    		if(setarr[i] == item) {
-    			return true;
-    		}
-    	}
-    	return false;
-    }
-    public String toString() {
-    	if(size == 0) {
-    		return "{}";
-    	}
-    	else {
-    		String str = "";
-    		str = str + "{";
-    		int i = 0;
-    		for(i = 0; i < size - 1; i++) {
-    			str = str + setarr[i] + ", ";
-    		}
-    		str = str + setarr[i] + "}";
-    		return str; 
-    	}
-    }
-    public void add(int item) {
-    	if(size == setarr.length) {
-    		resize();
-    	}
-    	if(!(contains(item))) {
-    		setarr[size] = item;
-    		size++;
-    	}
-    }
-    public void add(int[] items) {
-    	for(int i = 0; i < items.length; i++) {
-    		add(items[i]);
-    	}
-    }
-    public Set intersection(Set items) {
-    	Set newsetarr = new Set();
-    	for(int i = 0; i < size; i++) {
-    		if(items.contains(setarr[i])) {
-                 newsetarr.add(setarr[i]);
-    		}
-    	}
-    	return newsetarr;
-    }
-    public Set retainAll(int[] items) {
-    	Set newsetarr = new Set();
-    	for(int i = 0; i < size; i++) {
-    		for(int j = 0; j < items.length; j++) {
-    			if(setarr[i] == items[j]) {
-    				newsetarr.add(setarr[i]);
-    				break;
-    			}
-    		}
-    	}
-    	return newsetarr;
-    }
-    public int[][] cartesianProduct(Set other) {
-    	if(size == 0 || other.size == 0) {
-    		return null;
-    	}
-    	int[][] cartarr = new int[size][other.size];
-    	for(int i = 0; i < size; i++) {
-    		for(int j = 0; j < other.size; j++) {
-    			int[] testarr = new int[2];
-    			testarr[0] = setarr[i];
-    			testarr[1] = other.setarr[j];
-    			cartarr[i][j] = j;
-    		}
-    	}
-    	return cartarr;
-    }
+	//your code goes here...
+	//Good luck :-)
+	private int[] setarr;
+	private int size;
+	public Set() {
+		final int ten = 10;
+		setarr = new int[ten];
+		size = 0;
+	}
+	private void resize() {
+		setarr = Arrays.copyOf(setarr, 2 * setarr.length);
+	}
+	public int size() {
+		return size;
+	}
+	public boolean contains(int item) {
+		for (int i = 0; i < size; i++) {
+			if (setarr[i] == item) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public String toString() {
+		if (size == 0) {
+			return "{}";
+		} else {
+			String str = "";
+			str = str + "{";
+			int i = 0;
+			for (i = 0; i < size - 1; i++) {
+				str = str + setarr[i] + ", ";
+			}
+			str = str + setarr[i] + "}";
+			return str;
+		}
+	}
+	public void add(int item) {
+		if (size == setarr.length) {
+			resize();
+		}
+		if (!(contains(item))) {
+			setarr[size] = item;
+			size++;
+		}
+	}
+	public void add(int[] items) {
+		for (int i = 0; i < items.length; i++) {
+			add(items[i]);
+		}
+	}
+	public Set intersection(Set items) {
+		Set newsetarr = new Set();
+		for (int i = 0; i < size; i++) {
+			if (items.contains(setarr[i])) {
+				newsetarr.add(setarr[i]);
+			}
+		}
+		return newsetarr;
+	}
+	public Set retainAll(int[] items) {
+		Set newsetarr = new Set();
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < items.length; j++) {
+				if (setarr[i] == items[j]) {
+					newsetarr.add(setarr[i]);
+					break;
+				}
+			}
+		}
+		return newsetarr;
+	}
+	public int[][] cartesianProduct(Set other) {
+		if (size == 0 || other.size == 0) {
+			return null;
+		}
+		int[][] cartarr = new int[size*other.size][2];
+		  for (int i = 0; i < size; i++) {
+			for (int j = 0; j < other.size; j++) {
+					cartarr[i][0] = setarr[i];
+					cartarr[i][1] = other.setarr[j];
+				}
+			}
+		return cartarr;
+	}
 }
 /**
  * Solution class for code-eval.
  */
 public final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
 
-    }
-    /**
-     * helper function to convert string input to int array.
-     *
-     * @param      s     { string input from test case file }
-     *
-     * @return     { int array from the given string }
-     */
-    public static int[] intArray(final String s) {
-        String input = s;
-        if (input.equals("[]")) {
-            return new int[0];
-        }
-        if (s.contains("[")) {
-            input = s.substring(1, s.length() - 1);
-        }
-        return Arrays.stream(input.split(","))
-                            .mapToInt(Integer::parseInt)
-                            .toArray();
-    }
-    /**
-     * main function to execute test cases.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        // instantiate this set
-        Set s = new Set();
-        // code to read the test cases input file
-        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
-        // check if there is one more line to process
-        while (stdin.hasNext()) {
-            // read the line
-            String line = stdin.nextLine();
-            // split the line using space
-            String[] tokens = line.split(" ");
-            // based on the list operation invoke the corresponding method
-            switch (tokens[0]) {
-                case "size":
-                System.out.println(s.size());
-                break;
-                case "contains":
-                System.out.println(s.contains(Integer.parseInt(tokens[1])));
-                break;
-                case "print":
-                System.out.println(s);
-                break;
-                case "add":
-                int[] intArray = intArray(tokens[1]);
-                if (intArray.length == 1) {
-                    s.add(intArray[0]);
-                } else {
-                    s.add(intArray);
-                }
-                break;
-                case "intersection":
-                s = new Set();
-                Set t = new Set();
-                intArray = intArray(tokens[1]);
-                s.add(intArray);
-                intArray = intArray(tokens[2]);
-                t.add(intArray);
-                System.out.println(s.intersection(t));
-                break;
-                case "retainAll":
-                s = new Set();
-                intArray = intArray(tokens[1]);
-                s.add(intArray);
-                intArray = intArray(tokens[2]);
-                System.out.println(s.retainAll(intArray));
-                break;
-                case "cartesianProduct":
-                s = new Set();
-                t = new Set();
-                intArray = intArray(tokens[1]);
-                s.add(intArray);
-                intArray = intArray(tokens[2]);
-                t.add(intArray);
-                System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
-                break;
-                default:
-                break;
-            }
-        }
-    }
+	}
+	/**
+	 * helper function to convert string input to int array.
+	 *
+	 * @param      s     { string input from test case file }
+	 *
+	 * @return     { int array from the given string }
+	 */
+	public static int[] intArray(final String s) {
+		String input = s;
+		if (input.equals("[]")) {
+			return new int[0];
+		}
+		if (s.contains("[")) {
+			input = s.substring(1, s.length() - 1);
+		}
+		return Arrays.stream(input.split(","))
+		       .mapToInt(Integer::parseInt)
+		       .toArray();
+	}
+	/**
+	 * main function to execute test cases.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		// instantiate this set
+		Set s = new Set();
+		// code to read the test cases input file
+		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+		// check if there is one more line to process
+		while (stdin.hasNext()) {
+			// read the line
+			String line = stdin.nextLine();
+			// split the line using space
+			String[] tokens = line.split(" ");
+			// based on the list operation invoke the corresponding method
+			switch (tokens[0]) {
+			case "size":
+				System.out.println(s.size());
+				break;
+			case "contains":
+				System.out.println(s.contains(Integer.parseInt(tokens[1])));
+				break;
+			case "print":
+				System.out.println(s);
+				break;
+			case "add":
+				int[] intArray = intArray(tokens[1]);
+				if (intArray.length == 1) {
+					s.add(intArray[0]);
+				} else {
+					s.add(intArray);
+				}
+				break;
+			case "intersection":
+				s = new Set();
+				Set t = new Set();
+				intArray = intArray(tokens[1]);
+				s.add(intArray);
+				intArray = intArray(tokens[2]);
+				t.add(intArray);
+				System.out.println(s.intersection(t));
+				break;
+			case "retainAll":
+				s = new Set();
+				intArray = intArray(tokens[1]);
+				s.add(intArray);
+				intArray = intArray(tokens[2]);
+				System.out.println(s.retainAll(intArray));
+				break;
+			case "cartesianProduct":
+				s = new Set();
+				t = new Set();
+				intArray = intArray(tokens[1]);
+				s.add(intArray);
+				intArray = intArray(tokens[2]);
+				t.add(intArray);
+				System.out.println(Arrays.deepToString(s.cartesianProduct(t)));
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
