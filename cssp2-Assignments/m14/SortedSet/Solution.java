cb744879e69fcb3942cc222be9c0a2f893ec6d5c
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-10 17:50:54 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cssp2-Assignments/m14/SortedSet/Solution.java
+++ cssp2-Assignments/m14/SortedSet/Solution.java
@@ -339,7 +339,9 @@ final class Solution {
                 String[] argtokens = tokens[1].split(",");
                 if ((inputset.subSet(Integer.parseInt(argtokens[0]),
                                      Integer.parseInt(argtokens[1]))) != null) {
-                    System.out.println(toString(inputset.subSet(Integer.parseInt(argtokens[0]), Integer.parseInt(argtokens[1]))));
+                    System.out.println(toString(inputset.subSet(
+                                                    Integer.parseInt(argtokens[0]),
+                                                    Integer.parseInt(argtokens[1]))));
                 }
                 break;
             case "headSet":
