6be727da17a1c48aaec1189959a9370dfdf1f744
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-12 17:50:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m16/bookyourshow/Solution.java
+++ cssp2-Assignments/m16/bookyourshow/Solution.java
@@ -20,27 +20,27 @@ final class Solution {
         while (input.hasNext()) {
             String line = input.nextLine();
             String[] tokens = line.split(" ", 2);
-            switch(tokens[0]) {
-                case "add":
-                           String[] par = tokens[1].split(",", 3); 
-                           bookshow.add(par[0], par[1], par[2]);
-                           break;
-                case "book":
-                           par = tokens[1].split(",", 5);
-                           bookshow.book(par[0], par[1], par[2], par[3], par[4]);
-                           break;
-                case "print":
-                           par = tokens[1].split(",");
-                           System.out.println(bookshow.print(par[0], par[1], par[2]));
-                           break;
-                case "get":
-                           par = tokens[1].split(",");
-                           System.out.println(bookshow.get(par[0], par[1]));
-                           break;
-                case "showAll":
-                           bookshow.showAll();
-                           break;
-                default:
+            switch (tokens[0]) {
+            case "add":
+                String[] par = tokens[1].split(",", 3);
+                bookshow.add(par[0], par[1], par[2]);
+                break;
+            case "book":
+                par = tokens[1].split(",", 5);
+                bookshow.book(par[0], par[1], par[2], par[3], par[4]);
+                break;
+            case "print":
+                par = tokens[1].split(",");
+                System.out.println(bookshow.print(par[0], par[1], par[2]));
+                break;
+            case "get":
+                par = tokens[1].split(",");
+                System.out.println(bookshow.get(par[0], par[1]));
+                break;
+            case "showAll":
+                bookshow.showAll();
+                break;
+            default:
             }
         }
     }
