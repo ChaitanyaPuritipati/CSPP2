c83892d643242d13ffc025786a96f577c6821aaa
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-12 16:52:57 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m16/bookyourshow/BookYourShow.java
+++ cssp2-Assignments/m16/bookyourshow/BookYourShow.java
@@ -55,6 +55,7 @@ class BookYourShow {
 	String print(String moviename, String moviedate, String mobile) {
 		System.out.println(k + "value of k at the end of looping");
 		for (int i = 0; i < k - 1; i++) {
+			System.out.println(bookdetails[i][0] + "----" + bookdetails[i][1] + "------" + bookdetails[i][3] +"----");
 			if (bookdetails[i][0].equals(moviename) && bookdetails[i][1].equals(moviedate) && bookdetails[i][3].equals(mobile)) {
 				return mobile + " " + moviename + " " + moviedate;
 			}
