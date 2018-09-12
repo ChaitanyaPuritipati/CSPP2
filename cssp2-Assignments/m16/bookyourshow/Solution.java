594049cf619fdb02ff4f6a2cb05efb792d5f5af0
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-12 17:46:34 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m16/bookyourshow/Solution.java
+++ cssp2-Assignments/m16/bookyourshow/Solution.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution() {
-		//unused.
-	}
-	public static void main(String[] args) {
-		BookYourShow bookshow = new BookYourShow();
-		Scanner input = new Scanner(System.in);
-		while (input.hasNext()) {
-			String line = input.nextLine();
-			String[] tokens = line.split(" ", 2);
-			switch(tokens[0]) {
-				case "add":
-				           String[] par = tokens[1].split(",", 3); 
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+    }
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        BookYourShow bookshow = new BookYourShow();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            String[] tokens = line.split(" ", 2);
+            switch(tokens[0]) {
+                case "add":
+                           String[] par = tokens[1].split(",", 3); 
                            bookshow.add(par[0], par[1], par[2]);
                            break;
                 case "book":
@@ -28,8 +38,8 @@ class Solution {
                            break;
                 case "showAll":
                            bookshow.showAll();
-                           break;           
-			}
-		}
-	}
+                           break;
+            }
+        }
+    }
 }
