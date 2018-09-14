4b961ff97b2881a1538299ad0369c65cbde8fb54
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-14 10:33:50 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m17/Shoppingcart/Shoppingcart.java
+++ cssp2-Assignments/m17/Shoppingcart/Shoppingcart.java
@@ -16,22 +16,32 @@ class Shoppingcart {
 	void cartresize() {
 		cartitems = Arrays.copyOf(cartitems, 2 * cartsize);
 	}
-    void addtocatalog(item argitem) {
-    	if(catsize == catitems.length) {
-    		catresize();
-    	}
-    	catitems[catsize] = argitem;
-    	System.out.println(catitems[catsize]);
-    	catsize++;
-
-    }
-    void addtocart(item argitem) {
-    	if(cartsize == cartitems.length) {
-    		cartresize();
-    	}
-    	cartitems[cartsize] = argitem;
-    	System.out.println(cartitems[cartsize]);
-    	cartsize++;
-    }
+	void addtocatalog(item argitem) {
+		if (catsize == catitems.length) {
+			catresize();
+		}
+		catitems[catsize] = argitem;
+		catsize++;
+	}
+	void addtocart(item argitem) {
+		if (cartsize == cartitems.length) {
+			cartresize();
+		}
+		cartitems[cartsize] = argitem;
+		cartsize++;
+	}
+	int indexof(item argitem) {
+		for (int i = 0; i < cartsize; i++) {
+			if (cartitems[i].proname.equals(argitem.proname)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	void removefromcart(item argitem) {
+		for (int i = indexof(argitem); i < cartsize; i++) {
+			cartitems[i] = cartitems[i + 1];
+		}
+	}
 
 }
--- cssp2-Assignments/m17/Shoppingcart/Solution.java
+++ cssp2-Assignments/m17/Shoppingcart/Solution.java
@@ -2,30 +2,42 @@ import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
 		Shoppingcart shop = new Shoppingcart();
-        Scanner input = new Scanner(System.in);
-        while(input.hasNext()) {
-        	String line = input.nextLine();
-        	String[] tokens = line.split(" ");
-        	switch(tokens[0]) {
-        		case "Item":
-        		            String[] par = tokens[1].split(",");
-        		            item inpitem = new item(par[0], Integer.parseInt(par[1]), Integer.parseInt(par[2]));
-        		            shop.addtocatalog(inpitem);
-        		            break;
-        		case "add":
-        		            par = tokens[1].split(",");
-        		            int price = 0;
-        		            for(item catitem : shop.catitems) {
-        		            	if(catitem.proname.equals(par[0])) {
-                                     price = catitem.prounitprice;
-                                     break;
-        		            	}
-        		            }
-        		            inpitem = new item(par[0], Integer.parseInt(par[1]), price);
-        		            shop.addtocart(inpitem);
-        		            break;
-        		default:                        
-        	}
-        }
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
+			String line = input.nextLine();
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+			case "Item":
+				String[] par = tokens[1].split(",");
+				item inpitem = new item(par[0], Integer.parseInt(par[1]), Integer.parseInt(par[2]));
+				shop.addtocatalog(inpitem);
+				break;
+			case "add":
+				par = tokens[1].split(",");
+				int price = 0;
+				for (item catitem : shop.catitems) {
+					if (catitem.proname.equals(par[0])) {
+						price = catitem.prounitprice;
+						break;
+					}
+				}
+				inpitem = new item(par[0], Integer.parseInt(par[1]), price);
+				shop.addtocart(inpitem);
+				break;
+			case "remove":
+				par = tokens[1].split(",");
+				price = 0;
+				for (item catitem : shop.catitems) {
+					if (catitem.proname.equals(par[0])) {
+						price = catitem.prounitprice;
+						break;
+					}
+				}
+				inpitem = new item(par[0], Integer.parseInt(par[1]), price);
+				shop.removefromcart(inpitem);
+				break;
+			default:
+			}
+		}
 	}
 }
