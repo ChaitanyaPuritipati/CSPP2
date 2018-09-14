37118405c5d1f3c4999f208eb996f62585d78b14
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-14 12:10:47 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cssp2-Assignments/m17/Shoppingcart/Shoppingcart.java
+++ cssp2-Assignments/m17/Shoppingcart/Shoppingcart.java
@@ -6,11 +6,13 @@ class Shoppingcart {
 	item[] catitems;
 	String[] coupons = new String[4];
 	double discamt = 0;
+	int coucnt;
 	Shoppingcart() {
 		catitems = new item[10];
 		cartitems = new item[10];
 		catsize = 0;
 		cartsize = 0;
+		coucnt = 0;
 		coupons[0] = "IND10";
 		coupons[1] = "IND20";
 		coupons[2] = "IND30";
@@ -72,30 +74,38 @@ class Shoppingcart {
 	}
 	void applycoupon(String couponcode) {
 		int flag = 0;
+		if(coucnt > 1) {
+			return;
+		}
 		for (int i = 0; i < 4; i++) {
 			if (coupons[i].equals(couponcode)) {
 				flag = 1;
 			}
 		}
 		if (flag == 0) {
+			System.out.println("Invalid Coupon");
 			return;
 		}
 		switch (couponcode) {
 		case "IND10":
 			coupons[0] = "N/A";
 			discamt = discamt + (totalamount() * 0.1);
+			coucnt++;
 			break;
 		case "IND20":
 			coupons[1] = "N/A";
 			discamt = discamt + (totalamount() * 0.2);
+			coucnt++;
 			break;
 		case "IND30":
 			coupons[2] = "N/A";
 			discamt = discamt + (totalamount() * 0.3);
+			coucnt++;
 			break;
 		case "IND50":
 			coupons[3] = "N/A";
 			discamt = discamt + (totalamount() * 0.5);
+			coucnt++;
 			break;
 		default:
 		}
