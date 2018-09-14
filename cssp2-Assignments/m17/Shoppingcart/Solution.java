5eed58105a099ec577e60182b28c3bda8e128e2f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-14 14:21:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cssp2-Assignments/m17/Shoppingcart/Solution.java
+++ cssp2-Assignments/m17/Shoppingcart/Solution.java
@@ -2,74 +2,76 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * { Main function starts here }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Shoppingcart shop = new Shoppingcart();
-		Scanner input = new Scanner(System.in);
-		while (input.hasNext()) {
-			String line = input.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "Item":
-				String[] par = tokens[1].split(",");
-				item inpitem = new item(par[0], Integer.parseInt(par[1]), Double.parseDouble(par[2]));
-				shop.addtocatalog(inpitem);
-				break;
-			case "add":
-				par = tokens[1].split(",");
-				double price = 0;
-				for (item catitem : shop.catitems) {
-					if (catitem.proname.equals(par[0])) {
-						price = catitem.prounitprice;
-						break;
-					}
-				}
-				inpitem = new item(par[0], Integer.parseInt(par[1]), price);
-				shop.addtocart(inpitem);
-				break;
-			case "remove":
-				par = tokens[1].split(",");
-				price = 0;
-				for (item catitem : shop.catitems) {
-					if (catitem.proname.equals(par[0])) {
-						price = catitem.prounitprice;
-						break;
-					}
-				}
-				inpitem = new item(par[0], Integer.parseInt(par[1]), price);
-				shop.removefromcart(inpitem);
-				break;
-			case "catalog":
-				shop.showcatalog();
-				break;
-			case "show":
-				shop.showcart();
-				break;
-			case "totalAmount":
-				System.out.println("totalAmount: " + shop.totalamount());
-				break;
-			case "payableAmount":
-				System.out.println("Payable amount: " + shop.payableamount());
-				break;
-			case "coupon":
-				shop.applycoupon(tokens[1]);
-				break;
-			case "print":
-				shop.print();
-				break;
-			default:
-			}
-		}
-	}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main function starts here }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Shoppingcart shop = new Shoppingcart();
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "Item":
+                String[] par = tokens[1].split(",");
+                item inpitem = new item(par[0],
+                                        Integer.parseInt(par[1]),
+                                        Double.parseDouble(par[2]));
+                shop.addtocatalog(inpitem);
+                break;
+            case "add":
+                par = tokens[1].split(",");
+                double price = 0;
+                for (item catitem : shop.catitems) {
+                    if (catitem.proname.equals(par[0])) {
+                        price = catitem.prounitprice;
+                        break;
+                    }
+                }
+                inpitem = new item(par[0], Integer.parseInt(par[1]), price);
+                shop.addtocart(inpitem);
+                break;
+            case "remove":
+                par = tokens[1].split(",");
+                price = 0;
+                for (item catitem : shop.catitems) {
+                    if (catitem.proname.equals(par[0])) {
+                        price = catitem.prounitprice;
+                        break;
+                    }
+                }
+                inpitem = new item(par[0], Integer.parseInt(par[1]), price);
+                shop.removefromcart(inpitem);
+                break;
+            case "catalog":
+                shop.showcatalog();
+                break;
+            case "show":
+                shop.showcart();
+                break;
+            case "totalAmount":
+                System.out.println("totalAmount: " + shop.totalamount());
+                break;
+            case "payableAmount":
+                System.out.println("Payable amount: " + shop.payableamount());
+                break;
+            case "coupon":
+                shop.applycoupon(tokens[1]);
+                break;
+            case "print":
+                shop.print();
+                break;
+            default:
+            }
+        }
+    }
 }
