9a4347e504262b350b6f9a706ba50c2b3cdcefeb
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-15 10:57:34 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

---
+++ cssp2-Assignments/Module 19/m19/Assignment-1/Quiz.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+class question {
+	String questionname;
+	int maxmarks;
+	String choices;
+	int anschoice;
+	int penaltymarks; 
+	question(String name, String choicestr, int choice, int marks, int penalty) {
+        questionname = name;
+        maxmarks = marks;
+        choices = choicestr;
+        anschoice = choice;
+        penaltymarks = penalty;
+	}
+}
+class Quiz {
+	question[] questions = new question[4];
+	int questioncount;
+	String[] userchoices = new String[4];
+	int userchoicecount; 
+	Quiz() {
+		questioncount = 0;
+		userchoicecount = 0;
+	}
+	void quesresize() {
+		questions = Arrays.copyOf(questions, 2 * questioncount);
+	}
+	void userresize() {
+		userchoices = Arrays.copyOf(userchoices, 2 * userchoicecount);
+	}
+	void addtoques(question inputques) {
+		if(questioncount == questions.length) {
+			quesresize();
+		}
+		questions[questioncount] = inputques;
+		System.out.println(questions[questioncount].questionname + "(" + questions[questioncount].maxmarks + ")");
+		String[] displaychoices = questions[questioncount].choices.split(",");
+		for(int i = 0; i < displaychoices.length - 1; i++) {
+			System.out.print(displaychoices[i] + "   ");
+		}
+		System.out.print(displaychoices[displaychoices.length-1]);
+        System.out.println();
+	}
+	void addtochoices(String userchoice) {
+		if(userchoicecount == userchoices.length) {
+			userresize();
+		}
+		userchoices[userchoicecount++] = userchoice;
+	}
+}
