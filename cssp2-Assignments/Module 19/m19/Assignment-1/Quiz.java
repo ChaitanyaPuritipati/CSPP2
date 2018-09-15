27cc503171ec80b17f697a825f9eaa09b410909b
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-15 13:09:08 +0530

    "check_style_score -> 12 of 13 passed. style score: 1/1 "

--- cssp2-Assignments/Module 19/m19/Assignment-1/Quiz.java
+++ cssp2-Assignments/Module 19/m19/Assignment-1/Quiz.java
@@ -63,17 +63,6 @@ class Quiz {
 	}
 	int calculatescore() {
 		for (int i = 0; i < questioncount; i++) {
-			if (userchoices[i].split(" ")[1].equals("1") || userchoices[i].split(" ")[1].equals("2") || userchoices[i].split(" ")[1].equals("3") || userchoices[i].split(" ")[1].equals("4")) {
-				if (questions[i].anschoice.equals("1")) {
-					questions[i].anschoice = questions[i].choices.split(",")[0].split(" ")[1];
-				} else if (questions[i].anschoice.equals("2")) {
-					questions[i].anschoice = questions[i].choices.split(",")[1].split(" ")[1];
-				} else if (questions[i].anschoice.equals("3")) {
-					questions[i].anschoice = questions[i].choices.split(",")[2].split(" ")[1];
-				} else {
-					questions[i].anschoice = questions[i].choices.split(",")[3].split(" ")[1];
-				}
-			}
 			if (userchoices[i].split(" ")[1].equals("a") || userchoices[i].split(" ")[1].equals("b") || userchoices[i].split(" ")[1].equals("c") || userchoices[i].split(" ")[1].equals("d")) {
 				if (questions[i].anschoice.equals("1")) {
 					questions[i].anschoice = "a";
@@ -85,6 +74,28 @@ class Quiz {
 					questions[i].anschoice = "d";
 				}
 			}
+			if (userchoices[i].split(" ")[1].equals("1") || userchoices[i].split(" ")[1].equals("2") || userchoices[i].split(" ")[1].equals("3") || userchoices[i].split(" ")[1].equals("4")) {
+				if (questions[i].anschoice.equals("1")) {
+					questions[i].anschoice = questions[i].choices.split(",")[0].split(" ")[1];
+				} else if (questions[i].anschoice.equals("2")) {
+					questions[i].anschoice = questions[i].choices.split(",")[1].split(" ")[1];
+				} else if (questions[i].anschoice.equals("3")) {
+					questions[i].anschoice = questions[i].choices.split(",")[2].split(" ")[1];
+				} else {
+					questions[i].anschoice = questions[i].choices.split(",")[3].split(" ")[1];
+				}
+			}
+			// if (userchoices[i].split(" ")[1].equals("a") || userchoices[i].split(" ")[1].equals("b") || userchoices[i].split(" ")[1].equals("c") || userchoices[i].split(" ")[1].equals("d")) {
+			// 	if (questions[i].anschoice.equals("1")) {
+			// 		questions[i].anschoice = "a";
+			// 	} else if (questions[i].anschoice.equals("2")) {
+			// 		questions[i].anschoice = "b";
+			// 	} else if (questions[i].anschoice.equals("3")) {
+			// 		questions[i].anschoice = "c";
+			// 	} else {
+			// 		questions[i].anschoice = "d";
+			// 	}
+			// }
 			if (userchoices[i].split(" ")[1].equals("1") || userchoices[i].split(" ")[1].equals("2") || userchoices[i].split(" ")[1].equals("3") || userchoices[i].split(" ")[1].equals("4")) {
 				if (questions[i].anschoice.equals("1")) {
 					questions[i].anschoice = questions[i].choices.split(",")[0].split(" ")[1];
