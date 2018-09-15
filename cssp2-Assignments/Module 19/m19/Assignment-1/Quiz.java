a396a53eed2083ab2513471dd0a66664acdbbf26
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-15 15:30:09 +0530

    "check_style_score -> 13 of 13 passed. style score: 1/1 "

--- cssp2-Assignments/Module 19/m19/Assignment-1/Quiz.java
+++ cssp2-Assignments/Module 19/m19/Assignment-1/Quiz.java
@@ -63,17 +63,17 @@ class Quiz {
 	}
 	int calculatescore() {
 		for (int i = 0; i < questioncount; i++) {
-			// if (userchoices[i].split(" ")[1].equals("a") || userchoices[i].split(" ")[1].equals("b") || userchoices[i].split(" ")[1].equals("c") || userchoices[i].split(" ")[1].equals("d")) {
-			// 	if (questions[i].anschoice.equals("1")) {
-			// 		questions[i].anschoice = "a";
-			// 	} else if (questions[i].anschoice.equals("2")) {
-			// 		questions[i].anschoice = "b";
-			// 	} else if (questions[i].anschoice.equals("3")) {
-			// 		questions[i].anschoice = "c";
-			// 	} else {
-			// 		questions[i].anschoice = "d";
-			// 	}
-			// }
+			if (userchoices[i].split(" ")[1].equals("a") || userchoices[i].split(" ")[1].equals("b") || userchoices[i].split(" ")[1].equals("c") || userchoices[i].split(" ")[1].equals("d")) {
+				if (questions[i].anschoice.equals("1")) {
+					questions[i].anschoice = "a";
+				} else if (questions[i].anschoice.equals("2")) {
+					questions[i].anschoice = "b";
+				} else if (questions[i].anschoice.equals("3")) {
+					questions[i].anschoice = "c";
+				} else {
+					questions[i].anschoice = "d";
+				}
+			}
 			// if (userchoices[i].split(" ")[1].equals("1") || userchoices[i].split(" ")[1].equals("2") || userchoices[i].split(" ")[1].equals("3") || userchoices[i].split(" ")[1].equals("4")) {
 			// if (questions[i].choices.split(",").length == 2) {
 			// 	if (questions[i].anschoice.equals("1")) {
