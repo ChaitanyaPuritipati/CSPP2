6e5971fbb0d1f05ec69ee04a88e9eb8cf5487a62
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-15 11:08:14 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cssp2-Assignments/Module 19/m19/Assignment-1/Solution.java
+++ cssp2-Assignments/Module 19/m19/Assignment-1/Solution.java
@@ -65,7 +65,8 @@ public final class Solution {
         for(int i = 0; i < questionCount; i++) {
             String textline = s.nextLine();
             String[] tokens = textline.split(":");
-            if(tokens.length != 5  || tokens[0] == null || tokens[1] == null || tokens[2] == null || tokens[3] == null || tokens[4] == null) {
+            // System.out.println(tokens[0]);
+            if(tokens.length != 5  || tokens[0].equals(null) || tokens[1].equals(null) || tokens[2].equals(null) || tokens[3].equals(null) || tokens[4].equals(null)) {
                 System.out.println("Error Malformed Question!");
                 return;
             }
