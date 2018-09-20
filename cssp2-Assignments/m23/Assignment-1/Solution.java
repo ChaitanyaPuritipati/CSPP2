4a8c9b322ea0c4d69974808388214aecff64ef1a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-20 20:06:50 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cssp2-Assignments/m23/Assignment-1/Solution.java
+++ cssp2-Assignments/m23/Assignment-1/Solution.java
@@ -1,8 +1,21 @@
 import java.util.Scanner;
 import java.io.File;
-// import java.net.*;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	Solution() {
+		//unused
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		try {
 			double max = 0;
 			String file1 = "";
@@ -16,39 +29,26 @@ public class Solution {
 				return;
 			}
 			System.out.print("\t" + "\t");
-			for(File file : files) {
+			for (File file : files) {
 				System.out.print(file.getName() + "\t");
 			}
 			System.out.println();
 			for (File file : files) {
-                System.out.print(file.getName() + "\t");
+				System.out.print(file.getName() + "\t");
 				for (File eachfile : files) {
 					Distance map1 = new Distance(file);
-					// System.out.println(map1.getNumOfLines() + " lines  ");
-					// System.out.println(map1.getNumOfWords() + " words  ");
-					// System.out.println(map1.getNumOfDistinctWords() + " distinct words");
-					// System.out.println(map1.norm() + " Euclidean norm\n");
-
 					Distance map2 = new Distance(eachfile);
-					// System.out.println(map2.getNumOfLines() + " lines  ");
-					// System.out.println(map2.getNumOfWords() + " words  ");
-					// System.out.println(map2.getNumOfDistinctWords() + " distinct words");
-					// System.out.println(map2.norm() + " Euclidean norm\n");
-
-					// System.out.println(map2.innerProduct(map1.getMap()) + " inner product");
 					System.out.print(map1.angle(map2.getMap()) + "\t" + "\t");
 					if (map1.angle(map2.getMap()) > max && !(file.getName().equals(eachfile.getName()))) {
 						max = map1.angle(map2.getMap());
 						file1 = file.getName();
 						file2 = eachfile.getName();
-
 					}
 				}
 				System.out.println();
 			}
-			System.out.println("Maximum similarity is between " + file1 +" and "+ file2);
-		}
-		catch(Exception e) {
+			System.out.println("Maximum similarity is between " + file1 + " and " + file2);
+		} catch (Exception e) {
 			System.out.println("empty directory");
 			return;
 		}
