f67fd2bec24ede35570afc45dca2060ba3dbb191
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-20 20:08:25 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cssp2-Assignments/m23/Assignment-1/Solution.java
+++ cssp2-Assignments/m23/Assignment-1/Solution.java
@@ -4,54 +4,54 @@ import java.io.File;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		//unused
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		try {
-			double max = 0;
-			String file1 = "";
-			String file2 = "";
-			Scanner scan = new Scanner(System.in);
-			String dirname = scan.nextLine();
-			File dir = new File(dirname);
-			File[] files = dir.listFiles();
-			if (files.length == 0) {
-				System.out.println("empty directory");
-				return;
-			}
-			System.out.print("\t" + "\t");
-			for (File file : files) {
-				System.out.print(file.getName() + "\t");
-			}
-			System.out.println();
-			for (File file : files) {
-				System.out.print(file.getName() + "\t");
-				for (File eachfile : files) {
-					Distance map1 = new Distance(file);
-					Distance map2 = new Distance(eachfile);
-					System.out.print(map1.angle(map2.getMap()) + "\t" + "\t");
-					if (map1.angle(map2.getMap()) > max && !(file.getName().equals(eachfile.getName()))) {
-						max = map1.angle(map2.getMap());
-						file1 = file.getName();
-						file2 = eachfile.getName();
-					}
-				}
-				System.out.println();
-			}
-			System.out.println("Maximum similarity is between " + file1 + " and " + file2);
-		} catch (Exception e) {
-			System.out.println("empty directory");
-			return;
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        try {
+            double max = 0;
+            String file1 = "";
+            String file2 = "";
+            Scanner scan = new Scanner(System.in);
+            String dirname = scan.nextLine();
+            File dir = new File(dirname);
+            File[] files = dir.listFiles();
+            if (files.length == 0) {
+                System.out.println("empty directory");
+                return;
+            }
+            System.out.print("\t" + "\t");
+            for (File file : files) {
+                System.out.print(file.getName() + "\t");
+            }
+            System.out.println();
+            for (File file : files) {
+                System.out.print(file.getName() + "\t");
+                for (File eachfile : files) {
+                    Distance map1 = new Distance(file);
+                    Distance map2 = new Distance(eachfile);
+                    System.out.print(map1.angle(map2.getMap()) + "\t" + "\t");
+                    if (map1.angle(map2.getMap()) > max && !(file.getName().equals(eachfile.getName()))) {
+                        max = map1.angle(map2.getMap());
+                        file1 = file.getName();
+                        file2 = eachfile.getName();
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("Maximum similarity is between " + file1 + " and " + file2);
+        } catch (Exception e) {
+            System.out.println("empty directory");
+            return;
+        }
+    }
 
 }
