96bb310f5907a09af5feb444015364cf6d2c473a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-09-23 10:32:18 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- cssp2-Assignments/m24/Bagofwords/Distance.java
+++ cssp2-Assignments/m24/Bagofwords/Distance.java
@@ -60,7 +60,7 @@ public class Distance {
 	void buildingdict() {
 		for (String eachline : filelines) {
 			// eachline = eachline.toLowerCase();
-			eachline = eachline.replaceAll("[^a-z]"," ");
+			eachline = eachline.replaceAll("[^a-zA-Z]"," ");
 			// eachline = eachline.replaceAll("\\s+"," ");
 			// if (eachline.length() > 0) {
 				// System.out.println(eachline);
